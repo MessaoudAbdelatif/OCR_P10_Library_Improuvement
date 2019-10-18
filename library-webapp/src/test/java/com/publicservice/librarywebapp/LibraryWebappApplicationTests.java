1a9aef0abe7402dbd157e1c2cba77d84a3355412
MessaoudAbdelatif <dzdevsoft@gmail.com>
2019-10-19 00:46:58 +0200

    added library-webapp

---
+++ library-webapp/src/test/java/com/publicservice/librarywebapp/LibraryWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicservice.librarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
